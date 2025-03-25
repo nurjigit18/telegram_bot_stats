5b182857923ac5c4633354db36b25ea03b43913c
nurjigit18 <nurzhigitkurmanov@gmail.com>
2025-03-25 14:31:15 +0800

    missed changed

--- config.py
+++ config.py
@@ -6,14 +6,14 @@ import os
 
 # Get environment variables
 BOT_TOKEN = os.getenv('TELEGRAM_TOKEN')
-GOOGLE_CREDS_FILE = '/home/nurjigit18/telegrambot/utils/spheric-keel-430513-g9-f3948761d754.json'
+GOOGLE_CREDS_FILE = os.getenv('GOOGLE_CREDS_JSON')
 SHEET_ID = os.getenv('SHEET_ID')
 
 # Validate environment variables
 if not all([BOT_TOKEN, GOOGLE_CREDS_FILE, SHEET_ID]):
     raise ValueError("Please set TELEGRAM_TOKEN, GOOGLE_CREDS_FILE, and SHEET_ID in your .env file")
 
-ADMIN_USER_USERNAMES = ["nKurm", "AlinaK2205"]  # Replace with actual admin user IDs
+ADMIN_USER_USERNAMES = ["nKurm"]  # Replace with actual admin user IDs
 
 SIZE_PATTERN = r'([a-zA-Z]+)\s*:\s*(\d+)'
 
